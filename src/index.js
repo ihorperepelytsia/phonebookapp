917de6fce2d54472be78c73c28054e154d846072
Ihor Perepelytsia <igorekperepelitsa@gmail.com>
2022-06-30 15:18:10 +0300

    chande base name

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/phonebookapp/">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
